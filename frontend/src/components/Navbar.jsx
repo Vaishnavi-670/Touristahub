8b51f8a0f52cc10305bef81e7e56ceaeec7b5798
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-09-09 03:37:09 +0530

    .

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,7 +16,8 @@ const Navbar = () => {
         </li>
       )
      }else{
-      return <>                   
+      return <>  
+                         
         <li className="nav-item">
           <NavLink className="nav-link" to="/signup">
             Signup
@@ -46,8 +47,7 @@ const Navbar = () => {
            Destinations
             </NavLink>
             </li>
-            
-            
+          
       </>
      }
   }
@@ -55,17 +55,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg   bg-transparent overflow-y-hidden" >
   <div className="container-fluid">
     <img  style={{height:'60px', width:'100px', objectFit:'cover'}} src="https://media.istockphoto.com/id/1637056982/vector/travel-agency-business-logo-transport-logistics-delivery-logo-design.jpg?s=612x612&w=0&k=20&c=b1SGg7tQ9DGPDpzLvWJgqUl_LyrBzAQpabPw-RJfg5s=" alt="" />
-    {/* <button
-      className="navbar-toggler"
-      type="button"
-      // data-bs-toggle="collapse"
-      // data-bs-target="#navbarSupportedContent"
-      // aria-controls="navbarSupportedContent"
-      // aria-expanded="false"
-      // aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon" />
-    </button> */}
+    
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         
@@ -73,18 +63,6 @@ const Navbar = () => {
         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
            {showLoginOptions()}
         </ul>
-
-      {/* <form className="d-flex" role="search">
-        <input
-          className="form-control me-2"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        />
-        <button className="btn btn-outline-success" type="submit">
-          Search
-        </button>
-      </form> */}
     </div>
   </div>
 </nav>
