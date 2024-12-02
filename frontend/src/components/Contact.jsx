4798996a0eb0a4d0e3d41d580e0422d152121c64
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-12-02 23:43:16 +0530

    c

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -22,7 +22,6 @@ const Contact = () => {
             className="bg-light text-dark border-0 rounded p-2 mb-3 w-100 col-md-5 mr-md-2"
             placeholder="Phone Number"
           />
-
           <input
             type="text"
             className="bg-light text-dark border-0 rounded p-2 mb-3 w-100 col-md-5 ml-md-2"
